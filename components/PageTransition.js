50eeebb2c193a61772053b8f3b692c13d8c88280
kusucpu <kususikiwae@gmail.com>
2026-04-11 16:33:36 +0700

    Update PageTransition.js

--- components/PageTransition.js
+++ components/PageTransition.js
@@ -1,24 +1,12 @@
 'use client'
-import { motion, AnimatePresence } from 'framer-motion'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function PageTransition({ children }) {
   const pathname = usePathname()
-  
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{
-          duration: 0.3,
-          ease: 'easeInOut'
-        }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  )
+  const [key, setKey] = useState(pathname)
+
+  useEffect(() => { setKey(pathname) }, [pathname])
+
+  return <div key={key} className="pg-in">{children}</div>
 }
