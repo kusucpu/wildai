dafb75c5521ff819008a359487eb890272a65e4a
kusucpu <kususikiwae@gmail.com>
2026-04-11 16:17:24 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--bg-primary)',
-        foreground: 'var(--text-primary)',
-      },
-    },
-  },
+  content: ['./app/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: { extend: {} },
   plugins: [],
-  darkMode: 'class',
 }
